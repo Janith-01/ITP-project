eb753cfc9b1fd7aea857b76204cc7e949578dd31
Janith-viranga <128822354+Janiyah-01@users.noreply.github.com>
2024-05-11 00:18:24 +0530

    Delete Janith directory

--- Janith/Backend/routes/totalAmountRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import TotalIncomeController from '../controllers/TotalIncomeController.js'; 
-
-const router3 = express.Router();
-
-router3.post('/totalIncome', TotalIncomeController.saveTotalIncome);
-router3.get('/api/totalIncome', TotalIncomeController.getTotalIncome); 
-
-export default router3;
