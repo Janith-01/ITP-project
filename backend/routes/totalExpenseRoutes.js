cf2b8acbc213e43eb6c60d30f33a5ba4a0e00993
Janith-viranga <128822354+Janiyah-01@users.noreply.github.com>
2024-05-14 16:55:28 +0530

    Delete backend directory

--- backend/routes/totalExpenseRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import TotalExpenseController from '../controllers/TotalExpenseController.js';
-
-const router4 = express.Router();
-
-router4.get('/api/totalExpense', TotalExpenseController.getTotalExpense);
-
-export default router4;
