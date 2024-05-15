93272f2a0dd00e1fe64109a54a2975483f22e8e6
hashijayarathna <147933342+hashijayarathna@users.noreply.github.com>
2024-05-15 11:52:33 +0530

    env1
    env1

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -1,41 +1,18 @@
-/*const express = require("express");
-const mongoose = require("mongoose");
 
-const app = express();
-
-//MIddleware
-app.use("/", (req, res, next) => {
-    res.send("It Is Working...!");
-}) 
-
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://admin:2VTPq0EoXgEmJadp@cluster0.ww9dafb.mongodb.net/");
-    console.log("Connected to MongoDB!");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit process with failure
-  }
-};*/
-
-
-
-import express from "express";
+/*import express from "express";
 import mongoose from "mongoose";
 
-const app = express();
+//const app = express();
 
 // Middleware
 app.use("/", (req, res, next) => {
     res.send("It Is Working...!");
-}); 
+});*/
 
-const connectDB = async () => {
+/*const connectDB = async () => {
   try {
     await mongoose.connect("mongodb+srv://admin:2VTPq0EoXgEmJadp@cluster0.ww9dafb.mongodb.net/");
-    console.log("Connected to MongoDB!");
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1); // Exit process with failure
@@ -45,5 +22,5 @@ const connectDB = async () => {
 // Call connectDB function
 connectDB();
 
-export default app;
+export default app;*/
 
