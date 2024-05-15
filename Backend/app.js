d4d94906745b0402f1bc847353703dceb148f3b1
hashijayarathna <147933342+hashijayarathna@users.noreply.github.com>
2024-05-15 12:05:40 +0530

    backend changes2env
    backend changes2env

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import mongoose from "mongoose";
 import vehicleRouter from "./Routes/VehicleRoutes.js";
 import serviceHistoryRouter from "./Routes/ServiceHistoryRoutes.js";
@@ -22,7 +22,46 @@ mongoose
   .catch((err) => {
     console.error("MongoDB connection error:", err);
     process.exit(1); // Exit process with failure
+  });*/
+
+
+
+
+  import express from "express";
+import mongoose from "mongoose";
+import vehicleRouter from "./Routes/VehicleRoutes.js";
+import serviceHistoryRouter from "./Routes/ServiceHistoryRoutes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use("/vehicles", vehicleRouter);
+app.use("/serviceshistory", serviceHistoryRouter);
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit process with failure
   });
+
 
 
   
